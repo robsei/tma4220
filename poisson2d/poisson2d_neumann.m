ef08e30d9b0629bc01f5dcb19ffb87b9a88238b3
Robert Seidel <robersei@banach01.math.ntnu.no>
2016-11-11 16:21:32 +0100

    Fixed tratment of Neumann boundarz conditions and added test case.

--- poisson2d/poisson2d_neumann.m
+++ poisson2d/poisson2d_neumann.m
@@ -1,114 +1,81 @@
-clear all; clc; close all;
+function u_bd = poisson2d_neumann(p, tri, edge, f, g, eps)
 
 % Load needed functions.
 addpath('../quadrature');
-addpath('../grids');
 
-% Boundary conditions.
-f = @(X) 16 .* pi^2 .* X(1,:) .* X(2,:) .* (X(1,:).^2 + X(2,:).^2) .* sin(2.*pi.*(X(1,:).^2 + X(2,:).^2)) - 24.*X(1,:).*X(2,:).*pi.*cos(2.*pi.*(X(1,:).^2 + X(2,:).^2));
-g_1active = @(X) -X * sin(2*pi*X.^2);
-g_2active = @(X) X * sin(2*pi*X.^2);
+% Number of points.
+n = size(p, 1);
 
-% Grid construction.
-n = 500;
-theta = 3/2 * pi;
-[p, tri, edge] = getSlice(n, theta);
-
+% Number of elements.
 N = size(tri, 1);
 
-A = zeros(n,n);
+% Prepare linear system.
+A = sparse(n,n);
 b = zeros(n,1);
 
-triplot(tri, p(:,1), p(:,2));
+% Order of integration.
+Nq = 1;
 
-% Each triangle is visited once.
+% Get (Dirichlet) boundary nodes.
+e = unique(edge(:));
+
+% Visit each triangle once.
 for k = 1:N
-    % Get node ids to identify points and basis functions.
+    % Get node IDs to identify points and basis functions.
     nid = tri(k,:);
+    p1 = p(nid(1),:);
+    p2 = p(nid(2),:);
+    p3 = p(nid(3),:);
     
-    % Compute phi's that land in this triangle.
-    % Create lhs of the linear system.
+    % Compute PHIs that have support in this triangle.
+    % Create LHS of the linear system.
     lhs = [p(nid,:), ones(3,1)];
     phi = zeros(3,3);
     phi(:,1) = lhs \ [1; 0; 0];
     phi(:,2) = lhs \ [0; 1; 0];
     phi(:,3) = lhs \ [0; 0; 1];
     
-    % Is the triangle on the Neumann boundary?
-    is_on_edge = (abs(p(nid,1)) < eps & p(nid,2) < eps) | (p(nid,1) > eps & abs(p(nid,2)) < eps);
-    is_nm_edge = sum(is_on_edge) == 2;
-    if is_nm_edge
-        pt_nm_edge = p(nid(is_on_edge),:);
-        active_dim = sum(abs(pt_nm_edge),1) > (2*eps);
-    end
-    
-    
+    % Integrate and update A and b.
     for i = 1:3
-        p1 = p(nid(1),:);
-        p2 = p(nid(2),:);
-        p3 = p(nid(3),:);
-        Nq = 1;
-        for j = 1:3N = size(tri, 1);
-
-A = zeros(n,n);
-b = zeros(n,1);
-
-triplot(tri, p(:,1), p(:,2));
-
-% Each triangle is visited once.
-for k = 1:N
-    % Get node ids to identify points and basis functions.
-    nid = tri(k,:);
-    
-    % Compute phi's that land in this triangle.
-    % Create lhs of the linear system.
-    lhs = [p(nid,:), ones(3,1)];
-    phi = zeros(3,3);
-    phi(:,1) = lhs \ [1; 0; 0];
-    phi(:,2) = lhs \ [0; 1; 0];
-    phi(:,3) = lhs \ [0; 0; 1];
-    
-    % Is the triangle on the Neumann boundary?
-    is_on_edge = (abs(p(nid,1)) < eps & p(nid,2) < eps) | (p(nid,1) > eps & abs(p(nid,2)) < eps);
-    is_nm_edge = sum(is_on_edge) == 2;
-    if is_nm_edge
-        pt_nm_edge = p(nid(is_on_edge),:);
-        active_dim = sum(abs(pt_nm_edge),1) > (2*eps);
-    end
-    
-    
+        for j = 1:3
             g1 = @(x) dot(phi(1:2,i), phi(1:2,j)) * ones(1, size(x,2));
             A(nid(i),nid(j)) = A(nid(i),nid(j)) + quadrature2D(p1,p2,p3,Nq,g1);
         end
+        
         g2 = @(x) f(x) * phi(:,i)' * [x; ones(1, size(x,2))];
-        g3_1active = @(x) g_1active(x) * phi(:,i)' * [x; 0; 1];
-        g3_2active = @(x) g_2active(x) * phi(:,i)' * [0; x; 1];
         b(nid(i)) = b(nid(i)) + quadrature2D(p1,p2,p3,Nq,g2);
-        if is_nm_edge
-            if active_dim(1)
-                b(nid(i)) = b(nid(i)) + quadrature1D(pt_nm_edge(1,active_dim),pt_nm_edge(2,active_dim),Nq,g3_1active);
-            elseif active_dim(2)
-                b(nid(i)) = b(nid(i)) + quadrature1D(pt_nm_edge(1,active_dim),pt_nm_edge(2,active_dim),Nq,g3_2active);
-            end
+    end
+    
+    % Treatment of Neumann boundary conditions.
+    % Which points are on Omega_N?
+    is_bd_point = ...
+        (abs(p(nid,1)) < eps & p(nid,2) < eps & p(nid,2) > -1) ...
+             | ...
+        (p(nid,1) > eps & abs(p(nid,2)) < eps & p(nid,1) < 1);
+    
+    % If two points are on Omage_N, triangle has an edge with Neumann boundary.
+    has_nm_edge = sum(is_bd_point) == 2;
+    
+    % Update e and b, if necessary.
+    if has_nm_edge
+        nm_edge_nid = nid(is_bd_point);
+        nm_edge_pt = p(nm_edge_nid,:);
+    
+        for i = find(is_bd_point')
+            e(e == nid(i)) = [];
+            g3 = @(X) g(X) * phi(:,i)' * [X; 1];
+            b(nid(i)) = b(nid(i)) + linequadrature1D(nm_edge_pt(1,:),nm_edge_pt(2,:), Nq, g3);
         end
     end
 end
 
-A(edge(:,1),:) = [];
-A(:,edge(:,1)) = [];
-b(edge(:,1)) = [];
-u = A \ b;
-
-U = zeros(n,1);
+% Get indices of inner nodes and Neumann nodes.
 idx = 1:n;
-idx(edge(:,1)) = [];
-U(idx) = u;
+idx(e) = [];
 
-subplot(1,2,1);
-trimesh(tri, p(:,1), p(:,2), U);
+% Solve linear system on inner nodes and Neumann nodes.
+u = A(idx,idx) \ b(idx);
 
-% Verify the result.
-u_true = p(:,1) .* p(:,2) .* sin(2.*pi.*(p(:,1).^2 + p(:,2).^2));
-subplot(1,2,2);
-trimesh(tri, p(:,1), p(:,2), u_true);
-disp(['l2-error of solution is ', num2str(norm(U - u_true))]);
+% Fill in Dirichlet boundary condition.
+u_bd = zeros(n,1);
+u_bd(idx) = u;
