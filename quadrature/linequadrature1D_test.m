5b213a6c1cbcd66effd667a018c6378858d44c5a
Robert Seidel <robersei@banach01.math.ntnu.no>
2016-11-11 14:24:33 +0100

    Added line quadrature for Neumann boundary conditions.

---
+++ quadrature/linequadrature1D_test.m
@@ -0,0 +1,13 @@
+clear all; clc; close all;
+
+p1 = [0 0];
+p2 = [1 3];
+Nqs = 1:4;
+g = @(x) x(1,:).^2 + x(2,:).^4;
+
+I_true = 1/3 + (3^4)/5;
+
+for Nq = Nqs
+    I = linequadrature1D(p1,p2,Nq,g);
+    disp(['For Nq=' num2str(Nq) ': I = ' num2str(I) ', |I - I_true| = ' num2str(abs(I - I_true))]);
+end
