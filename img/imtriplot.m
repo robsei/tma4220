6e9283f0033431f27c862a4a4ef2dcac400a9aa6
Robert Seidel <robersei@banach03.math.ntnu.no>
2016-11-13 20:19:17 +0100

    Prepared img generation for report, i.e. U_true and runtime measure.

--- img/imtriplot.m
+++ img/imtriplot.m
@@ -6,7 +6,7 @@ plot(p(edge',1), p(edge',2), '-r', 'LineWidth', 2);
 
 view(90,90)
 axis off tight square
-title(['#nodes: ', num2str(size(p,1)), ' #elements: ', num2str(size(tri,1))]);
+% title(['#nodes: ', num2str(size(p,1)), ' #elements: ', num2str(size(tri,1))]);
 
 end
 
--- img/loadCameraman.m
+++ img/loadCameraman.m
@@ -1,12 +1,15 @@
-function U = loadCameraman(scale, sigma)
+function [U, U_true] = loadCameraman(scale, sigma)
 
 U = mat2gray(imread('cameraman.tif'));
 U = imresize(U, scale);
+U_true = U;
 
 if sigma > 0
     U = U + sigma * randn(size(U));
     U = max(min(U,1),0);
 end
+
+disp(['Loaded camerman with ' num2str(size(U)) ' pixels.'])
 
 end
 
